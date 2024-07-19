54e8ea7d43d7729188ebb828553722c3226d2c6c
Ravi Raj Meena <101081595+Balerion769@users.noreply.github.com>
2024-07-19 10:46:03 +0530

    Update paymentSchema.js
    removing errored code

--- models/paymentSchema.js
+++ models/paymentSchema.js
@@ -1,18 +1,3 @@
-// import mongoose from 'mongoose';
-// const { Schema } = mongoose;
-
-// const PaymentSchema = new Schema({
-//   user_id: { type: String, required: true },
-//   amount: { type: Number, required: true },
-//   razorpay_order_id: { type: String, required: true },
-//   razorpay_payment_id: { type: String },
-//   status: { type: String, default: 'created' },
-// });
-
-// const Payment = mongoose.model('Payment', PaymentSchema);
-
-// export default Payment;
-
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
